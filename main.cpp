28bf8e5d693b9f62bc8e6729dfd63c520f2f42fa
Han <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-04-04 10:45:18 +1300

    修改了在call CreateTree()时，即使用户输入-1，也持续让用户输入左节点和右节点的问题。 问题源于：在Leave()获取Queue的第一个元素时，没有将front后移，导致每次call Leave()都在获取第一个元素。因此造成无限循环。

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 
 using namespace std;
 
+// This node is for a tree node
+class Node {
+public:
+    Node *lchild, *rchild;
+    int data;
+};
+
+
 class Queue {
 private:
     int size;
     int front, rear;
-    int *Q;
+    // // This is an array address for integer
+    //int *Q;
+
+    // Now for creating a BT, we need to change the value type
+    Node **Q; // should store an address of a tree, so
 
 public:
     Queue();
@@ -15,11 +27,11 @@ public:
 
     ~Queue();
 
-    void Join(int data);
+    void Join(Node *node);
 
-    int Leave();
+    Node *Leave();
 
-    int Front();
+    Node *Front();
 
     bool isEmpty();
 
@@ -30,20 +42,23 @@ public:
 Queue::Queue() {
     this->size = 5;
     front = rear = 0;
-    Q = new int[this->size];
+    //Q = new int[this->size];
+    Q = new Node *[this->size];
 }
 
 Queue::Queue(int size) {
     this->size = size;
     front = rear = 0;
-    Q = new int[this->size];
+//    Q = new int[this->size];
+    Q = new Node *[size];
 }
 
 Queue::~Queue() {
 
 }
 
-void Queue::Join(int data) {
+// change int to Node
+void Queue::Join(Node *p) {
     // check if the array is full
     // 条件是用%，如果(rear +1) % size == front, 那就是已经满了
     if ((rear + 1) % size == front)
@@ -52,37 +67,35 @@ void Queue::Join(int data) {
         // 如果没有满，那可以继续添加，添加到(rear + 1) % size 的地方，来达到circular的目的
         rear = (rear + 1) % size;
         // 把data插进queue
-        Q[rear] = data;
+//        Q[rear] = data;
+        Q[rear] = p;
     }
 }
 
-int Queue::Leave() {
+Node *Queue::Leave() {
     // 先检查array是不是已经空了
     // empty的条件是：front == rear 吗
-    int returnData = -1;
-    if (front == rear) {
+    Node *temp = NULL;
+    if (isEmpty()) {
         cout << "The queue is empty, will delete(and return) nothing." << endl;
     } else {
         // 如果不是空的，那么删除这个front元素
         // front也要执行circular，用%计算新的位置
         front = (front + 1) % size;
-        returnData = Q[front];
+        temp = Q[front];
     }
-    return returnData;
-
+    return temp;
 }
 
-int Queue::Front() {
-    int x = -1;
+Node *Queue::Front() {
+    Node *p = NULL;
     if (isEmpty()) {
         cout << "Queue is empty" << endl;
+    } else {
+        p = Q[(front + 1) % size];
     }
-    else
-    {
-        x = Q[(front+1) %size];
-    }
-    return x;
-    
+    return p;
+
 
 }
 
@@ -104,18 +117,155 @@ void Queue::Display() {
 }
 
 
-int main() {
-    Queue Q;
-    Q.Join(10);
-    Q.Join(20);
-    Q.Join(30);
-    Q.Join(40); // 只能加到这，因为default是5，但是总有一个空的不能用，就是front指向的那个array
-    Q.Join(50);  // 失败
-    Q.Join(60); // 失败
-    Q.Display(); //10 20 30 40
+// class Tree
+class Tree {
+public:
+    Node *root;
 
-    Q.Leave();
-    Q.Display(); //20 30 40
+    Tree() { root = NULL; }
+
+    void CreateTree();
+
+    void Pre_order(Node *p);
+
+    void Post_order(Node *p);
+
+    void In_order(Node *p);
+
+    void Level_order(Node *p);
+
+    int Height(Node *p);
+};
+
+void Tree::CreateTree() {
+    Node *p, *t;
+    int x;
+    Queue q(10);
+    cout << "Enter root value " << endl;
+    string input;
+    getline(cin, input);
+    int int_input = stoi(input);// convert String => integer
+    // allocate memory to root from heap
+    this->root = new Node;
+    root->data = int_input;
+    root->lchild = root->rchild = NULL;
+    // push the root to Queue for the further using
+    // this step is an initialise step to create a tree
+    q.Join(root);
+
+    // the following steps are repeated steps to finish creating the tree
+    while (!q.isEmpty()) // when q is not empty:
+    {
+        // get a tree node from the queue, ask user to type in its left/ right child
+//        p = q.Front();
+        p = q.Leave();
+        cout << "Enter left child for this node: " << endl;
+        string lc;
+        getline(cin, lc);
+        int intlc = stoi(lc);
+
+        // check if user input is -1, that means there will be no left child for this node
+        // if input is not -1, then create a new node and add it to its parent's lchild.
+        if (intlc != -1) {
+            t = new Node;
+            t->data = intlc;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            // push this lchild to queue
+            q.Join(t);
+        }
+
+        // Ok, now it's right child's turn
+        cout << "Enter right child of this node:" << endl;
+        string rc;
+        getline(cin, rc);
+        int intrc = stoi(rc);
+        if (intrc != -1) {
+            t = new Node;
+            t->data = intrc;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            q.Join(t);
+        }
+
+    }
+
+
+}
+
+void Tree::Pre_order(Node *p) {
+    if (p) {
+        cout << p->data << " ";
+        Pre_order(p->lchild);
+        Pre_order(p->rchild);
+    }
+}
+
+void Tree::Post_order(Node *p) {
+    if (p) {
+        Pre_order(p->lchild);
+        Pre_order(p->rchild);
+        cout << p->data << " ";
+    }
+}
+
+void Tree::In_order(Node *p) {
+    if (p) {
+        Pre_order(p->lchild);
+        cout << p->data << " ";
+        Pre_order(p->rchild);
+    }
+}
+
+void Tree::Level_order(Node *p) {
+    Node *temp;
+    Queue q(20);
+    cout << p->data << " ";
+    q.Join(p);
+    while (!q.isEmpty()) {
+        temp = q.Front();
+        if (temp->lchild) {
+            cout << temp->lchild->data << " ";
+            q.Join(temp->lchild);
+        }
+
+        if (temp->rchild) {
+            cout << temp->rchild->data << endl;
+            q.Join(temp->rchild);
+        }
+    }
+}
+
+int Tree::Height(Node *p) {
+    int x = 0, y = 0;
+    if (!root)
+        return 0;
+    x = Height(p->lchild);
+    y = Height(p->rchild);
+    if (x > y)
+        return x + 1;
+    else
+        return y + 1;
+}
+
+
+int main() {
+//    Queue Q;
+//    Q.Join(10);
+//    Q.Join(20);
+//    Q.Join(30);
+//    Q.Join(40); // 只能加到这，因为default是5，但是总有一个空的不能用，就是front指向的那个array
+//    Q.Join(50);  // 失败
+//    Q.Join(60); // 失败
+//    Q.Display(); //10 20 30 40
+//
+//    Q.Leave();
+//    Q.Display(); //20 30 40
+
+// creating a BT
+    Tree t;
+    t.CreateTree();
+    t.Pre_order(t.root);
 
 
 }
